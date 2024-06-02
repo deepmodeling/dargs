2c6a79cabf2fc50eb80084734db96eae4d802fe4
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-06-01 23:59:56 -0400

    add tests for _convert_types
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- tests/test_json_schema.py
+++ tests/test_json_schema.py
@@ -5,7 +5,7 @@ import unittest
 
 from jsonschema import validate
 
-from dargs.json_schema import generate_json_schema
+from dargs.json_schema import generate_json_schema, _convert_types
 
 from .dpmdargs import example_json_str, gen_args
 
@@ -16,3 +16,15 @@ class TestJsonSchema(unittest.TestCase):
         schema = generate_json_schema(args)
         data = json.loads(example_json_str)
         validate(data, schema)
+
+    def test_convert_types(self):
+        self.assertEqual(_convert_types(int), "number")
+        self.assertEqual(_convert_types(str), "string")
+        self.assertEqual(_convert_types(float), "number")
+        self.assertEqual(_convert_types(bool), "boolean")
+        self.assertEqual(_convert_types(None), "null")
+        self.assertEqual(_convert_types(type(None)), "null")
+        self.assertEqual(_convert_types(list), "array")
+        self.assertEqual(_convert_types(dict), "object")
+        with self.assertRaises(ValueError):
+            _convert_types(set)
