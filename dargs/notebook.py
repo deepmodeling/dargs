153d668576585f3448243b1d2a179ec508b840d2
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-11-20 22:09:38 -0500

    fixed
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- dargs/notebook.py
+++ dargs/notebook.py
@@ -147,11 +147,14 @@ class ArgumentData:
         The data to be displayed.
     arg : Union[dargs.Argument, dargs.Variant]
         The Argument that describes the data.
+    repeat : bool, optional
+        The argument is repeat
     """
 
-    def __init__(self, data: dict, arg: Argument | Variant):
+    def __init__(self, data: dict, arg: Argument | Variant, repeat: bool = False):
         self.data = data
         self.arg = arg
+        self.repeat = repeat
         self.subdata = []
         self._init_subdata()
 
@@ -160,7 +163,7 @@ class ArgumentData:
         if (
             isinstance(self.data, dict)
             and isinstance(self.arg, Argument)
-            and not self.arg.repeat
+            and not (self.arg.repeat and not self.repeat)
         ):
             sub_fields = self.arg.sub_fields.copy()
             # extend subfiles with sub_variants
@@ -179,12 +182,21 @@ class ArgumentData:
                 else:
                     self.subdata.append(ArgumentData(self.data[kk], kk))
         elif (
-            isinstance(self.data, (list, dict))
+            isinstance(self.data, list)
             and isinstance(self.arg, Argument)
             and self.arg.repeat
+            and not self.repeat
         ):
             for dd in self.data:
-                self.subdata.append(ArgumentData(dd, self.arg))
+                self.subdata.append(ArgumentData(dd, self.arg, repeat=True))
+        elif (
+            isinstance(self.data, dict)
+            and isinstance(self.arg, Argument)
+            and self.arg.repeat
+            and not self.repeat
+        ):
+            for dd in self.data.values():
+                self.subdata.append(ArgumentData(dd, self.arg, repeat=True))
 
     def print_html(self, _level=0, _last_one=True):
         """Print the data with Argument in HTML format.
@@ -207,7 +219,7 @@ class ArgumentData:
         if _level > 0 and not (
             isinstance(self.data, dict)
             and isinstance(self.arg, Argument)
-            and self.arg.repeat
+            and self.repeat
         ):
             if isinstance(self.arg, (Argument, Variant)):
                 buff.append(r"""<span class="dargs-key">""")
